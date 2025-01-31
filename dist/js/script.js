3370227f1180c8be6a5ce8d508fdfa9328fe083a
danielashjari <70367201+danielashjari@users.noreply.github.com>
2025-01-31 10:57:14 +0330

    update script.js for mobile ver

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,33 @@
 const shareIcon = document.getElementById("share-icon");
-const profileInfo = document.getElementById("profile-Info");
+const profileName = document.getElementById("profile-name");
+const profileImage = document.getElementById("profile-image");
 const desktopBar = document.getElementById("share-desktop");
 const mobileBar = document.getElementById("share-mobile");
 
-shareIcon.addEventListener("click", function(){
-    if (window.innerWidth > 1024 ) {
-        if (desktopBar.style.display === "none") {
-            desktopBar.style.display = "flex";
-          } else {
-            desktopBar.style.display = "none";
-          }
+shareIcon.addEventListener("click", function () {
+  if (window.innerWidth > 1024) {
+    if (desktopBar.style.display === "none") {
+      desktopBar.style.display = "flex";
+    } else {
+      desktopBar.style.display = "none";
     }
+  }
 
-    
+
 })
 
-shareIcon.addEventListener("mouseover", function() {
-    if (window.innerWidth < 1024){
-        if (mobileBar.style.display === "none") {
-            mobileBar.style.display = "flex";
-          } else {
-            mobileBar.style.display = "none";
-          }
+shareIcon.addEventListener("mouseover", function () {
+  if (window.innerWidth < 1024) {
+    if (mobileBar.style.display === "none") {
+      profileName.style.display = "none";
+      profileImage.style.display = "none";
+      mobileBar.style.display = "flex";
+
+    } else {
+      profileName.style.display = "block";
+      profileImage.style.display = "block";
+      mobileBar.style.display = "none";
     }
+  }
 
 })
