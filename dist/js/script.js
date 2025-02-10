bd976845a2b083856592d70e2b4014be4927bfed
danielashjari <70367201+danielashjari@users.noreply.github.com>
2025-02-10 21:27:45 +0330

    commenting

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,6 @@
 const shareBtn = document.getElementById("share-btn");
 const shareContainer = document.getElementById("share-container");
-const shareWrapper = document.getElementById("share-wrapper");
 const profileInfo = document.getElementById("profile-info");
-const profileCard = document.getElementById("profile-card");
 
 shareBtn.addEventListener('click', function(){
     //event listener for mobile view
