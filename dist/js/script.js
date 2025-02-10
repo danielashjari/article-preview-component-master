eb6f54cc3912766f76595b713bf89cc6921b83df
danielashjari <70367201+danielashjari@users.noreply.github.com>
2025-02-10 18:58:24 +0330

    refactor share button event listener for responsive behavior

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,19 +4,28 @@ const shareWrapper = document.getElementById("share-wrapper");
 const profileInfo = document.getElementById("profile-info");
 const profileCard = document.getElementById("profile-card");
 
-
+if (window.innerWidth <=1020) {
 shareBtn.addEventListener('click', function(){
   shareContainer.classList.toggle('hidden');
   shareContainer.classList.toggle('flex');
-  if(window.innerWidth < 1080){
-    profileCard.classList.toggle('hidden');
-    profileCard.classList.toggle('flex');
-  }
+  profileInfo.classList.toggle('flex');
+  profileInfo.classList.toggle('hidden');
   
   if(shareContainer.classList.contains('flex')){
     shareBtn.setAttribute('aria-expanded', 'true');
   }else if(shareContainer.classList.contains('hidden')){
     shareBtn.setAttribute('aria-expanded', 'false');
   }
-})
+})}
+if (window.innerWidth >1020) {
+shareBtn.addEventListener('click', function(){
+  shareContainer.classList.toggle('hidden');
+  shareContainer.classList.toggle('flex');
+  
+  if(shareContainer.classList.contains('flex')){
+    shareBtn.setAttribute('aria-expanded', 'true');
+  }else if(shareContainer.classList.contains('hidden')){
+    shareBtn.setAttribute('aria-expanded', 'false');
+  }
+})}
 
